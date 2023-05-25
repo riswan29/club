75efd8992a9c230d8b646c2bafd682c44018ab7f
riswan29 <wandra290401@gmail.com>
2023-05-25 19:13:37 +0700

    membuat fungsi autentikasi untuk login dan menambahkan flash message django

--- base/views.py
+++ base/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from django.db.models import Q
+from django.contrib import messages
+from django.contrib.auth import login,logout,authenticate
 from .models import Room, Topic
 from .forms import RoomForm
 
 
 
 def loginPage(request):
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "User does not exist")
+        user = authenticate(request, username=username , password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Username Or Passoword doesn't exist")
+
+
     context = {}
     return render(request, 'base/login.html' ,context)
 
