6e883e50382bd4ebfd81c6eb919d3228a8369fb6
riswan29 <wandra290401@gmail.com>
2023-05-25 15:07:56 +0700

    menambahkan fitur buat room dan juga filter room

---
+++ base/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from base.views import *
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('room/<str:pk>/' ,room, name='room'),
+    path('create-room/', createRoom, name='create-room'),
+    path('update-room/<str:pk>/', updateRoom, name='update-room'),
+    path('delete-room/<str:pk>/', deleteRoom, name='delete-room'),
+]
