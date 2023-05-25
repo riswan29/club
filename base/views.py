1b9f20440b056d93ec8e1f70df03d4156b031a80
riswan29 <wandra290401@gmail.com>
2023-05-25 15:26:28 +0700

    membuat fungsi fitur search bar dengan library django Q

--- base/views.py
+++ base/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
 
 def home(request):
-    q = request.GET.get('q')
-    rooms = Room.objects.filter(topic__name=q)
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
     # rooms = Room.objects.all()
     topics = Topic.objects.all()
     context = {
