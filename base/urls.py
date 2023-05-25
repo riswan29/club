39b48f316977d0043d5637496348ad04ccfde020
riswan29 <wandra290401@gmail.com>
2023-05-25 22:10:36 +0700

    add link to register page

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,7 @@ from base.views import *
 
 urlpatterns = [
     path('login/', loginPage, name="login"),
+    path('register/', registerPage, name="register"),
     path('logout/', logoutUser, name="logout"),
     path('admin/', admin.site.urls),
     path('', home, name='home'),
